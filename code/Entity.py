1a59d7b7ba7ecb793b71a95e5f73768c3f157db2
Vinicius Paula <trabalho.rar@proton.me>
2025-10-13 15:32:16 -0300

    Creating mediator pattern

--- code/Entity.py
+++ code/Entity.py
@@ -11,6 +11,7 @@ class Entity(ABC):
         self.surf = pygame.image.load('./asset/' + name + '.png').convert_alpha()
         self.rect = self.surf.get_rect(left=position[0], top=position[1])
         self.speed = 0
+        self.health = ENTITY_HEALTH[self.name]
 
     @abstractmethod
     def move(self):
